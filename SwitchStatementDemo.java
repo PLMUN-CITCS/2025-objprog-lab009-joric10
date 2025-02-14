df86670dcb181b999b958bf77b17924a3607abbf
joric10 <balconjoric_bscs@plmun.edu.ph>
2025-02-14 18:37:24 +0800

    Submitting OBJPROG Week 04 - Session 01 - Exercise 04

--- SwitchStatementDemo.java
+++ SwitchStatementDemo.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class SwitchStatementDemo {
     public static void main(String[] args) {
 int dayOfWeek = 4;
@@ -54,5 +55,44 @@ switch (studentGrade) {
 
 
 
+=======
+import java.util.Scanner; // Import Scanner for user input
+
+public class SwitchStatementDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get day of the week from user
+        System.out.print("Enter day of the week (1-7): ");
+        int dayOfWeek = scanner.nextInt();
+
+        // Traditional Switch Statement
+        switch (dayOfWeek) {
+            case 1 -> System.out.println("Monday: Start of the work week.");
+            case 2 -> System.out.println("Tuesday: Keep going!");
+            case 3 -> System.out.println("Wednesday: Midweek.");
+            case 4 -> System.out.println("Thursday: Almost there.");
+            case 5 -> System.out.println("Friday: Weekend is near.");
+            case 6 -> System.out.println("Saturday: Enjoy your day off!");
+            case 7 -> System.out.println("Sunday: Rest and recharge.");
+            default -> System.out.println("Invalid day.");
+        }
+
+        // Get student grade from user
+        System.out.print("Enter your grade (A-F): ");
+        char studentGrade = scanner.next().charAt(0);
+
+        // Traditional Switch Statement
+        switch (Character.toUpperCase(studentGrade)) {
+            case 'A' -> System.out.println("Excellent!");
+            case 'B' -> System.out.println("Good job!");
+            case 'C' -> System.out.println("Well done!");
+            case 'D' -> System.out.println("You passed.");
+            case 'F' -> System.out.println("Better luck next time.");
+            default -> System.out.println("Invalid grade.");
+        }
+
+        scanner.close(); // Close Scanner
+>>>>>>> 46c3bcd (Submitting OBJPROG Week 04 - Session 01 - Exercise 04)
     }
 }
